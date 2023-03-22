7888e9a32a76940a19327e1b283fa1e3c7610227
AgustinPalmaM <palma.agustin7@gmail.com>
2023-03-22 18:16:42 -0300

    create function searchImages(textSearch) to make fetch to the url with the textSearc argument Create function showImages(origin) to iterate over the response from fetch and build the container with the preview image, likes, views and link to the full resolution image Create funtion cleanHtml to clean preview results and render the news one

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import API_KEY from "../apiKey.js";
+
 const form = document.querySelector('#formulario');
 const result = document.querySelector('#resultado');
 
@@ -14,6 +16,8 @@ function validateForm(e) {
     showAlert("You should type a text to search some image");
     return
   }
+
+  searchImages(searchText);
 
 }
 
@@ -41,5 +45,77 @@ function showAlert(message) {
     setTimeout(() => {
       alert.remove();
     },1500)
+  }
+}
+
+function searchImages(textSearch) {
+  const key = API_KEY;
+  const url = `https://pixabay.com/api/?key=${key}&q=${textSearch}&per_page=100`;
+  
+  fetch(url)
+    .then(response => response.json())
+    .then(data => showImages(data.hits))
+}
+
+function showImages(origin) {
+  cleanHtml(result)
+  origin.forEach(element => {
+    const { previewURL, likes, views, largeImageURL } = element
+    const mainCointainer = document.createElement('DIV');
+    mainCointainer.classList.add('w-1/2', 'md:w-1/3', 'lg:w-1/4', 'p-3', 'mb-4');
+    
+    const div = document.createElement('DIV');
+    div.classList.add('bg-white');
+
+    const img = document.createElement('IMG');
+    img.classList.add('w-full');
+    img.src = previewURL;
+
+    const divInfoImage = document.createElement('DIV');
+    divInfoImage.classList.add('p-4');
+
+    const numLikes = document.createElement('P');
+    numLikes.classList.add('font-bold')
+    numLikes.textContent = likes;
+
+    const textLikes = document.createElement('SPAN');
+    textLikes.classList.add('font-light')
+    textLikes.textContent = ' Likes';
+
+    const numViews = document.createElement('P');
+    numViews.classList.add('font-bold')
+    numViews.textContent = views;
+
+    const textViews = document.createElement('SPAN');
+    textViews.classList.add('font-light')
+    textViews.textContent = ' Views';
+
+    const linkLargeImage = document.createElement('A');
+    linkLargeImage.classList.add('block','w-full', 'bg-blue-800','hover:bg-blue-500', 'text-white', 'uppercase', 'font-bold', 'text-center', 'rounded', 'mt-5', 'p-1')
+    linkLargeImage.textContent = 'View max resolution';
+    linkLargeImage.href = largeImageURL;
+    linkLargeImage.target = '_blank';
+    linkLargeImage.rel = 'noopener noreferrer';
+
+    numLikes.appendChild(textLikes);
+    numViews.appendChild(textViews);
+    divInfoImage.appendChild(numLikes);
+    divInfoImage.appendChild(numViews);
+    divInfoImage.appendChild(linkLargeImage);
+
+    div.appendChild(img);
+    div.appendChild(divInfoImage);
+
+    mainCointainer.appendChild(div);
+
+    result.appendChild(mainCointainer);
+  
+
+  });
+}
+
+function cleanHtml(element) {
+  while(element.firstChild) {
+    element.firstChild.remove()
   }
 }
